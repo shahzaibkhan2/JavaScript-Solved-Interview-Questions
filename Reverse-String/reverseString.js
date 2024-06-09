0682c549446f08813932f1481fb631b4773bc3a1
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-06-09 14:15:58 +0500

    Reverse a whole string or part of string ( word )

--- Reverse-String/reverseString.js
+++ Reverse-String/reverseString.js
@@ -1,1 +1,16 @@
+const reverseWord = (str) => {
+  const tempStr = str.split("").reverse().join("");
 
+  return tempStr;
+};
+
+console.log(reverseWord("king"));
+
+const reverseString = (str) => {
+  const tempStr = str.split(" ").map((word) => {
+    return word.split("").reverse().join("");
+  });
+  return tempStr.join(" ");
+};
+
+console.log(reverseString("king is back hurry"));

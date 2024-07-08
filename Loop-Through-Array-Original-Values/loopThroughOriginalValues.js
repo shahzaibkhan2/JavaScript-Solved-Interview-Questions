7d4ccb6b64a568875a407dfa164973b947819f4c
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-07-08 21:18:55 +0500

    Loop Through Array Original Values
    Loop Through Array Original Values

--- Loop-Through-Array-Original-Values/loopThroughOriginalValues.js
+++ Loop-Through-Array-Original-Values/loopThroughOriginalValues.js
@@ -1,1 +1,18 @@
+const tempArray = [1, 2, 3, 4, 5, 6];
 
+tempArray.prototype.newValue1 = "new Value";
+tempArray.prototype.newValue2 = "new Value 2";
+
+// Common approach which will cause it's new values in the prototype to also loop through
+for (let value in tempArray) {
+  console.log(value);
+}
+
+// More accurate approach to loop through original values and ignoring the prototype values
+// hasOwnProperty() method will only mark arrays original values to be true.
+
+for (let value in tempArray) {
+  if (tempArray.hasOwnProperty(value)) {
+    console.log(value);
+  }
+}
